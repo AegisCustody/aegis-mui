42f72f744b465f5f5254f5589d40c37bc99f18ba
Mason <jeailfy9520@outlook.com>
2023-03-05 22:33:23 +0800

    style: set button's default style

--- theme/componentOptions.ts
+++ theme/componentOptions.ts
@@ -1,6 +1,20 @@
 import { Components, Theme } from '@mui/material'
 
 const componentOptions: Components<Omit<Theme, 'components'>> | undefined = {
+  MuiButton: {
+    styleOverrides: {
+      root: {
+        minHeight: '40px',
+        boxShadow: 'none',
+        '&:hover': {
+          boxShadow: 'none',
+        },
+        '&.MuiButton-containedPrimary': {
+          color: 'white',
+        },
+      },
+    },
+  },
   MuiButtonBase: {
     styleOverrides: {
       root: {
