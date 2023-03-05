584b86af16c8cc373e5c728583c5fe18d46f8041
Mason <jeailfy9520@outlook.com>
2023-03-05 23:16:58 +0800

    style(Typography): add title variant

--- stories/Page.tsx
+++ stories/Page.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import { Typography } from '../components'
+import { Stack, Typography } from '../components'
 
 const Page: React.FC = () => {
   return (
-    <section>
-      <Typography>Material-UI Components</Typography>
-    </section>
+    <>
+      <Typography variant="title">Typography：</Typography>
+      <Stack>
+        <Typography variant="h1">h1</Typography>
+        <Typography variant="h2">h2</Typography>
+        <Typography variant="h3">h3</Typography>
+        <Typography variant="h4">h4</Typography>
+        <Typography variant="h5">h5</Typography>
+        <Typography variant="h6">h6</Typography>
+        <Typography variant="subtitle1">subtitle1</Typography>
+        <Typography variant="subtitle2">subtitle2</Typography>
+        <Typography variant="body1">body1</Typography>
+        <Typography variant="body2">body2</Typography>
+        <Typography variant="button">button</Typography>
+        <Typography variant="caption">caption</Typography>
+        <Typography variant="overline">overline</Typography>
+      </Stack>
+    </>
   )
 }
 
--- theme/typographyOptions.ts
+++ theme/typographyOptions.ts
@@ -3,10 +3,30 @@ import { TypographyOptions } from '@mui/material/styles/createTypography'
 
 const typographyOptions: TypographyOptions | ((palette: Palette) => TypographyOptions) | undefined =
   {
+    title: {
+      fontSize: '32px',
+      fontWeight: 700,
+    },
     button: {
       fontWeight: 'bold',
       textTransform: 'none',
     },
   }
+
+declare module '@mui/material/styles' {
+  interface TypographyVariants {
+    title: React.CSSProperties
+  }
+
+  interface TypographyVariantsOptions {
+    title?: React.CSSProperties
+  }
+}
+
+declare module '@mui/material/Typography' {
+  interface TypographyPropsVariantOverrides {
+    title: true
+  }
+}
 
 export default typographyOptions
