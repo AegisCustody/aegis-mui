a021aab6380028e4e6d3bae8bdc6c5956593329d
Mason <jeailfy9520@outlook.com>
2023-03-05 20:31:27 +0800

    chore: combine type and module

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,4 @@
-import type { ButtonProps } from '@mui/material/Button'
-import MuiButton from '@mui/material/Button'
+import MuiButton, { ButtonProps } from '@mui/material/Button'
 
 const Button: React.FC<ButtonProps> = props => {
   return <MuiButton {...props} />
