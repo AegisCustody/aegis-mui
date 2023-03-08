f166498db9ae4dd6f46ed4f1e2967a1132dac84a
Mason <jeailfy9520@outlook.com>
2023-03-08 16:08:46 +0800

    style: styled Dialog component

--- components/Dialog.tsx
+++ components/Dialog.tsx
@@ -1,23 +1,25 @@
 import MuiDialog, { DialogProps } from '@mui/material/Dialog'
-import MuiDialogContent from '@mui/material/DialogContent'
+import DialogContent from '@mui/material/DialogContent'
 import { styled } from '@mui/material/styles'
 import React from 'react'
 
-const StyledDialogContent = styled(MuiDialogContent)(({ theme }) => ({
-  display: 'grid',
-  gap: '30px',
-  padding: '60px',
-  textAlign: 'center',
-  [theme.breakpoints.down('md')]: {
-    padding: '30px',
+const StyledDialog = styled(MuiDialog)(({ theme }) => ({
+  '.MuiDialogContent-root': {
+    display: 'grid',
+    gap: '30px',
+    padding: '60px',
+    textAlign: 'center',
+    [theme.breakpoints.down('md')]: {
+      padding: '30px',
+    },
   },
 }))
 
 const Dialog: React.FC<DialogProps> = ({ children, ...props }) => {
   return (
-    <MuiDialog fullWidth {...props}>
-      <StyledDialogContent>{children}</StyledDialogContent>
-    </MuiDialog>
+    <StyledDialog fullWidth {...props}>
+      <DialogContent>{children}</DialogContent>
+    </StyledDialog>
   )
 }
 
