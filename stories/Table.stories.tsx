f7ebf10cc0311e9fe18b155c7f953c189c08e12b
Mason <jeailfy9520@outlook.com>
2023-03-14 10:08:07 +0800

    feat(Table): import Config type

--- stories/Table.stories.tsx
+++ stories/Table.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { Table } from '../components'
+import { Config } from '../components/Table/Table'
 import Page from './Page'
 import ThemeTemplate from './ThemeTemplate'
 
@@ -8,27 +9,25 @@ export default {
   component: Table,
 } as ComponentMeta<typeof Table>
 
-export const Template: ComponentStory<typeof Table> = args => {
-  const configs = [
-    {
-      header: 'Header 1',
-      render: () => (
-        <>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur</>
-      ),
-    },
-    { header: 'Header 2', render: () => <>Header 2</> },
-    { header: 'Header 3', render: () => <>Header 3</> },
-    { header: 'Header 4' },
-    { header: 'Header 5' },
-    { header: 'Header 6' },
-    { header: 'Header 7', render: () => <>Header 7</> },
-    { header: 'Header 8', render: () => <>Header 8</> },
-    { header: 'Header 9', render: () => <>Header 9</> },
-  ]
+const configs: Config[] = [
+  {
+    header: 'Header 1',
+    render: () => (
+      <>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur</>
+    ),
+  },
+  { header: 'Header 2', render: () => <>Header 2</> },
+  { header: 'Header 3', render: () => <>Header 3</> },
+  { header: 'Header 4' },
+  { header: 'Header 5' },
+  { header: 'Header 6' },
+  { header: 'Header 7', render: () => <>Header 7</> },
+  { header: 'Header 8', render: () => <>Header 8</> },
+  { header: 'Header 9', render: () => <>Header 9</> },
+]
 
-  return (
-    <ThemeTemplate
-      component={<Table {...args} configs={configs} list={[{}, {}, {}]} rowExpansion={Page} />}
-    />
-  )
-}
+export const Template: ComponentStory<typeof Table> = args => (
+  <ThemeTemplate
+    component={<Table {...args} configs={configs} list={[{}, {}, {}]} rowExpansion={Page} />}
+  />
+)
