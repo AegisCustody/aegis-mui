36a213c06006937c6bcff2b4eb740633c098ac36
Mason <jeailfy9520@outlook.com>
2023-03-15 01:29:25 +0800

    feat: add hint variant

--- components/Alert.tsx
+++ components/Alert.tsx
@@ -1,15 +1,21 @@
 import Box, { BoxProps } from '@mui/material/Box'
 import { styled } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
 import React from 'react'
 
 const StyledBox = styled(Box)(({ theme }) => ({
   padding: '20px',
   borderRadius: '6px',
+  color: theme.palette.primary.main,
   backgroundColor: theme.palette.background.default,
 }))
 
 const Alert: React.FC<BoxProps> = ({ children, ...props }) => {
-  return <StyledBox {...props}>{children}</StyledBox>
+  return (
+    <StyledBox {...props}>
+      <Typography variant="hint">{children}</Typography>
+    </StyledBox>
+  )
 }
 
 export default Alert
