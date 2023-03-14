b29ade7dcbfe7ad53c81f73863c12552e9307029
Mason <jeailfy9520@outlook.com>
2023-03-15 01:35:52 +0800

    style: set min width auto

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,6 +2,7 @@ import LoadingButton from '@mui/lab/LoadingButton'
 import { styled } from '@mui/material/styles'
 
 const Button = styled(LoadingButton)(({ theme }) => ({
+  minWidth: 'auto',
   minHeight: '40px',
   boxShadow: 'none',
   '&:hover': {
